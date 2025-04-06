fe5ca5a883a1398a0f68a793034ede4d1eda1787
Darela <85245283+LucasDarela@users.noreply.github.com>
2025-04-06 16:15:38 -0300

    added theme

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { ThemeSelector } from "@/components/theme-selector";
 import { ModeToggle } from "@/components/toggle-theme";
 
